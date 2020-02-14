63fcca571247768a1eac24e6af07ca9a12109d8d
CaKellum <cakellum@bsu.edu>
2020-02-14 17:03:54 -0500

    added tests

--- test/InsuranceCalculatorTest.java
+++ test/InsuranceCalculatorTest.java
@@ -4,4 +4,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class InsuranceCalculatorTest {
 
+    double calculate(double income){
+        return new InsuranceCalculator().calculateInsurance(income);
+    }
+
+    @Test
+    public void low() {
+        assertEquals(1825, calculate(5000), 0.01);
+    }
+
+    @Test
+    public void medium() {
+        assertEquals(38600, calculate(25000), 0.01);
+    }
+
+    @Test
+    public void high() {
+        assertEquals(78500, calculate(50000), 0.01);
+    }
+
+    @Test
+    public void veryHigh() {
+        assertEquals(106400, calculate(100_000), 0.01);
+    }
+
 }
