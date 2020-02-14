980ba136dadcc005e075cbeee466108116f59daf
CaKellum <cakellum@bsu.edu>
2020-02-14 17:08:12 -0500

    extracted methods

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -2,14 +2,30 @@ public class InsuranceCalculator {
 
     public double calculateInsurance(double income) {
         if (income <= 10000) {
-            return income*0.365;
+            return low(income);
         } else if (income <= 30000) {
-            return (income-10000)*0.2+35600;
+            return medium(income);
         } else if (income <= 60000) {
-            return (income-30000)*0.1+76500;
+            return high(income);
         } else {
-            return (income-60000)*0.02+105600;
+            return veryHigh(income);
         }
+    }
+
+    private double low(double income) {
+        return income*0.365;
+    }
+
+    private double medium(double income) {
+        return (income-10000)*0.2+35600;
+    }
+
+    private double high(double income) {
+        return (income-30000)*0.1+76500;
+    }
+
+    private double veryHigh(double income) {
+        return (income-60000)*0.02+105600;
     }
 
 }
